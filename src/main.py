6b8d5e049732f091970fda8d06d09e4f78cab1fe
Andrew <97903569+Andrew7210@users.noreply.github.com>
2025-02-03 18:51:33 -0500

    adding multiple log and monitor mechanisms (#6)

--- src/main.py
+++ src/main.py
@@ -3,13 +3,24 @@ import numpy as np
 from gymnasium.envs.registration import register
 from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback
+from stable_baselines3.common.monitor import Monitor
 from simglucose.analysis.risk import risk_index # type: ignore
+import torch
 
 IS_TRAINING = True
 
 # Set a fixed seed
 seed = 42
 np.random.seed(seed)
+
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+print(f"Using device: {device}")
 
 # Worth researching if there are more effective reward functions
 # maybe consider rate of change of bgl, though risk index may already accounts for this
@@ -30,23 +41,30 @@ register(
 )
 
 def train():
-    env = gymnasium.make("simglucose/adolescent2-v0", render_mode="human")
+    
+    env = gymnasium.make("simglucose/adolescent2-v0", render_mode=None, seed=seed)
+    env = Monitor(env, filename="monitor_logs/")
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.25 * np.ones(n_actions))
 
-    model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
-
-    model.learn(total_timesteps=10000, log_interval=10)
+    model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1, device = device,
+                 learning_rate=1e-3, buffer_size=100000, batch_size=256, gamma=0.99, tensorboard_log="./tb_logs/")
+    
+    eval_env = gymnasium.make("simglucose/adolescent2-v0", render_mode=None, seed=seed)
+    eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/", 
+                                 log_path="./logs/", eval_freq=5000, n_eval_episodes=5)
+    checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./checkpoints/', name_prefix='ddpg_checkpoint')
+    model.learn(total_timesteps=200000, callback=[eval_callback, checkpoint_callback])
 
     # Save trained model
     model.save("ddpg_simglucose")
     print("Model saved as 'ddpg_simglucose'.")
 
 def predict():
-    env = gymnasium.make("simglucose/adolescent2-v0", render_mode="human")
-
+    env = gymnasium.make("simglucose/adolescent2-v0", render_mode="human", seed=seed)
+    env = Monitor(env, filename="monitor_logs/")
     # Load the trained model
-    model = DDPG.load("ddpg_simglucose")
+    model = DDPG.load("ddpg_simglucose", device=device)
     print("Model loaded from 'ddpg_simglucose'.")
 
     observation, info = env.reset(seed=seed)
