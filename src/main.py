7d889034c517bd1d9293c451f6b8a2241342627b
y-mx <48138770+y-mx@users.noreply.github.com>
2025-02-14 10:51:52 -0500

    Add argparse to main (#7)
    * Add parsing args from provided yaml file for registering environment
    
    * Updated Train and Predict function so it uses YAML parameters
    
    ---------
    
    Co-authored-by: Yimeng Xie <yimeng.xie@cloudsparklabs.com>
    Co-authored-by: pPark <96594926+paulpark6@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -7,11 +7,14 @@ from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback
 from stable_baselines3.common.monitor import Monitor
 from simglucose.analysis.risk import risk_index # type: ignore
 import torch
+from cmd_args import parse_args
 
+args_dict = parse_args()
 IS_TRAINING = True
 
+
 # Set a fixed seed
-seed = 42
+seed = args_dict['seed']
 np.random.seed(seed)
 
 if torch.cuda.is_available():
@@ -34,40 +37,46 @@ def custom_reward_fn(BG_last_hour):
 
 # Register the custom environment
 register(
-    id="simglucose/adolescent2-v0",
-    entry_point="simglucose.envs:T1DSimGymnaisumEnv",
-    max_episode_steps=1000,
-    kwargs={"patient_name": "adolescent#002", 'reward_fun': custom_reward_fn},
+    id=args_dict['env']['id'],
+    entry_point=args_dict['env']['entry_point'],
+    max_episode_steps=args_dict['env']['max_episode_steps'],
+    kwargs={"patient_name": args_dict['env']['patient_name'], 'reward_fun': custom_reward_fn},
 )
 
-def train():
+def train(): 
     
-    env = gymnasium.make("simglucose/adolescent2-v0", render_mode=None, seed=seed)
+    env_config = args_dict["env"]
+    env = gymnasium.make(env_config["id"], render_mode=None, seed=args_dict["seed"])
     env = Monitor(env, filename="monitor_logs/")
     n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.25 * np.ones(n_actions))
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=args_dict["action_noise"]["sigma"] * np.ones(n_actions))
 
-    model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1, device = device,
-                 learning_rate=1e-3, buffer_size=100000, batch_size=256, gamma=0.99, tensorboard_log="./tb_logs/")
+    model_config = args_dict["model"]
+    model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1, device=args_dict["device"],
+                 learning_rate=model_config["learning_rate"], buffer_size=model_config["buffer_size"], 
+                 batch_size=model_config["batch_size"], gamma=model_config["gamma"], 
+                 tensorboard_log=args_dict["training"]["tensorboard_log"])
     
-    eval_env = gymnasium.make("simglucose/adolescent2-v0", render_mode=None, seed=seed)
-    eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/", 
-                                 log_path="./logs/", eval_freq=5000, n_eval_episodes=5)
-    checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./checkpoints/', name_prefix='ddpg_checkpoint')
-    model.learn(total_timesteps=200000, callback=[eval_callback, checkpoint_callback])
+    eval_env = gymnasium.make(env_config["id"], render_mode=None, seed=args_dict["seed"])
+    eval_callback = EvalCallback(eval_env, best_model_save_path=args_dict["training"]["log_path"],
+                                 log_path=args_dict["training"]["log_path"], eval_freq=args_dict["eval"]["eval_freq"], 
+                                 n_eval_episodes=args_dict["eval"]["n_eval_episodes"])
+    checkpoint_callback = CheckpointCallback(save_freq=args_dict["training"]["checkpoint_freq"], 
+                                             save_path=args_dict["training"]["save_path"], name_prefix='ddpg_checkpoint')
+    model.learn(total_timesteps=args_dict["training"]["total_timesteps"], callback=[eval_callback, checkpoint_callback])
 
     # Save trained model
     model.save("ddpg_simglucose")
     print("Model saved as 'ddpg_simglucose'.")
-
+        
 def predict():
-    env = gymnasium.make("simglucose/adolescent2-v0", render_mode="human", seed=seed)
+    env = gymnasium.make(args_dict["env"]["id"], render_mode="human", seed=args_dict["seed"])
     env = Monitor(env, filename="monitor_logs/")
     # Load the trained model
-    model = DDPG.load("ddpg_simglucose", device=device)
+    model = DDPG.load("ddpg_simglucose", device=args_dict["device"])
     print("Model loaded from 'ddpg_simglucose'.")
 
-    observation, info = env.reset(seed=seed)
+    observation, info = env.reset(seed=args_dict["seed"])
 
     for t in range(200):
         env.render()
